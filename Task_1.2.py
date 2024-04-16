201557f85140525ef7fb382722082dd0090192d6
ejones001 <ejonesnavy@gmail.com>
2024-04-16 13:30:03 -0500

    Task1.2

---
+++ Task_1.2.py
@@ -0,0 +1,8 @@
+#Show case your dance moves Task 2
+
+answer=input("How do you feel today?")
+
+if answer=="Happy":
+    print("That's great to hear")
+elif answer=="Sad":
+    print("I hope your day gets better")
