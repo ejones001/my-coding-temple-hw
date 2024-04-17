bd778c9f53791c719dcb5465bd6c24e39ba2c600
ejones001 <ejonesnavy@gmail.com>
2024-04-16 21:38:49 -0500

    Task4.2

---
+++ Task_4.2.py
@@ -0,0 +1,16 @@
+#Arithmetic Operations in Daily Life
+
+# Initial amount in the savings account
+initial_amount = 10000
+
+# Fixed yearly interest rate (in percentage)
+interest_rate = 7
+
+# Convert interest rate from percentage to decimal
+interest_rate_decimal = interest_rate / 100
+
+# Calculate the amount of interest earned
+interest_earned = initial_amount * interest_rate_decimal
+
+# Calculate the total amount in the account after a year
+total_amount = initial_amount + interest_earned
