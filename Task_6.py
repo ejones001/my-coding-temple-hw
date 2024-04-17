974d7c6c4da23c79633327e39816c4eeaedc4a84
ejones001 <ejonesnavy@gmail.com>
2024-04-16 21:52:19 -0500

    Task6Update

--- Task_6.py
+++ Task_6.py
@@ -1,6 +1,6 @@
 #Exploring Logical Operations and Precedence
-outcome1 = 5 * 3 + 2
-outcome2 = 5 * (3 + 2)
+outcome1 = 10 * 4 + 2
+outcome2 = 10 * (4 + 2)
 
 # Check if the results match
 if outcome1 == outcome2:
