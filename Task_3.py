c2d076933ae1731f44d167e5f4f7f67effb8876a
ejones001 <ejonesnavy@gmail.com>
2024-04-16 20:22:19 -0500

    Task3

---
+++ Task_3.py
@@ -0,0 +1,10 @@
+# Python data types and type () function
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print("Data type of variable a:",type(variable_a))
+print("Data type of variable b:",type(variable_b))
+print("Data type of variable c:",type(variable_c))
+print("Data type of variable d:",type(variable_d))
