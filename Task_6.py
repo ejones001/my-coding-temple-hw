5f680807d21a841595fd845287fea8f3aa0f87b2
ejones001 <ejonesnavy@gmail.com>
2024-04-16 21:50:22 -0500

    Task6

---
+++ Task_6.py
@@ -0,0 +1,9 @@
+#Exploring Logical Operations and Precedence
+outcome1 = 5 * 3 + 2
+outcome2 = 5 * (3 + 2)
+
+# Check if the results match
+if outcome1 == outcome2:
+    print("Results match!")
+else:
+    print("Results don't match.")
