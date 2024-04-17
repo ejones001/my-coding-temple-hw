de5d88361ad7d2df642adff5799e55db55e9f544
ejones001 <ejonesnavy@gmail.com>
2024-04-16 21:46:45 -0500

    Task5

---
+++ Task_5.py
@@ -0,0 +1,16 @@
+# Understanding Assignments and Comparisons
+num1 = 5
+num2 = 10
+
+# Swap values 
+num1, num2 = num2, num1
+
+# Compare to ensure they have been swapped
+print("number1 after swapping:", num1)
+print("number2 after swapping:", num2)
+
+# Comparison to ensure they have been swapped
+if num1 == 10 and num2 == 5:
+    print("Values have been successfully swapped.")
+else:
+    print("Values have not been swapped.")
